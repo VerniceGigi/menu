1ea4a3a3a4190b1040e151a26d1d9a53731c8967
VerniceGigi <83035283+VerniceGigi@users.noreply.github.com>
2021-04-27 06:18:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function dropit(element) {
+	var dropdowns = document.getElementsByClassName("dropitems");
+	// element.nextSibling is the carriage return… The dropdown menu is the next next.
+	var thisDropdown = element.nextSibling.nextSibling;
+
+
+	if (!thisDropdown.classList.contains('showit')) { 
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			dropdowns[i].classList.remove('showit');
+		}
+	}
+
+	// Toggle the dropdown on the element clicked
+	thisDropdown.classList.add("showit");
+}
+
+var btnIndex = 0;
+
+opacit(btnIndex);
+
+			
+function opac(m) {
+	opacit(btnIndex = m);
+}
+
+function opacit(m) {
+	var j;
+	var k;
+	var button = document.getElementsByClassName("dropbtn");
+
+	if (m > button.length) {btnIndex = 1}
+
+	if (m < 1) {btnIndex = button.length}
+
+	for (j = 0; j < button.length; j++) {
+		button[j].classList.remove('showdropbtn');
+	}
+
+	button[btnIndex-1].classList.add("showdropbtn");
+}
+
